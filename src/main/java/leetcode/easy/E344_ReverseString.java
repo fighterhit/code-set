198143030015d7fdfd18594abe0361c396027dc7
fighter <fighterhit@163.com>
2018-11-17 23:43:27 +0800

    init reverseString

---
+++ src/main/java/leetcode/easy/E344_ReverseString.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+/**
+ * Write a function that takes a string as input and returns the string reversed.
+ * <p>
+ * Example 1:
+ * Input: "hello"
+ * Output: "olleh"
+ * <p>
+ * Example 2:
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: "amanaP :lanac a ,nalp a ,nam A"
+ */
+public class E344_ReverseString {
+    public String reverseString(String s) {
+
+    }
+}
