8a2d0fbd742047f53e629e6f6f6e2a9ed04369b1
fighter <fighterhit@163.com>
2018-11-26 23:25:22 +0800

    add solution

--- src/main/java/leetcode/easy/E217_ContainsDuplicate.java
+++ src/main/java/leetcode/easy/E217_ContainsDuplicate.java
@@ -1,5 +1,8 @@
 package leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given an array of integers, find if the array contains any duplicates.
  * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
@@ -18,6 +21,16 @@ package leetcode.easy;
  */
 public class E217_ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-
+        if (nums == null || nums.length == 1) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            //向set加相同元素返回false
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
