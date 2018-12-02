f19de976aaab60df607dc216449259db8b2c3dd3
fighter <fighterhit@163.com>
2018-12-03 00:42:51 +0800

    fix solution

--- src/main/java/leetcode/easy/E219_ContainsDuplicateII.java
+++ src/main/java/leetcode/easy/E219_ContainsDuplicateII.java
@@ -1,5 +1,8 @@
 package leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
  * <p>
@@ -17,7 +20,42 @@ package leetcode.easy;
  */
 public class E219_ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
-        return true;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
     }
+   /* public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>(k);
+        if (nums.length < k) {
+            for (int i = 0; i < nums.length; i++) {
+                if (!set.add(nums[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        for (int i = 0; i < k; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            } else {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }*/
 }
