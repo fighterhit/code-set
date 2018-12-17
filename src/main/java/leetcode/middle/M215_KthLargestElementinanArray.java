b4a12c11adae2d5488d9e953d000f4a804058e91
fighter <fighterhit@163.com>
2018-12-17 23:54:23 +0800

    add top k solution

--- src/main/java/leetcode/middle/M215_KthLargestElementinanArray.java
+++ src/main/java/leetcode/middle/M215_KthLargestElementinanArray.java
@@ -1,16 +1,18 @@
 package leetcode.middle;
 
+import java.util.PriorityQueue;
+
 /**
  * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Example 1:
  * Input: [3,2,1,5,6,4] and k = 2
  * Output: 5
- *
+ * <p>
  * Example 2:
  * Input: [3,2,3,1,2,4,5,5,6] and k = 4
  * Output: 4
- *
+ * <p>
  * Note:
  * You may assume k is always valid, 1 ≤ k ≤ array's length.
  *
@@ -18,6 +20,18 @@ package leetcode.middle;
  */
 public class M215_KthLargestElementinanArray {
     public int findKthLargest(int[] nums, int k) {
-
+        //默认最小堆
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int num : nums) {
+            if (priorityQueue.size() < k) {
+                priorityQueue.add(num);
+            } else {
+                if (num > priorityQueue.peek()) {
+                    priorityQueue.poll();
+                    priorityQueue.add(num);
+                }
+            }
+        }
+        return priorityQueue.peek();
     }
 }
