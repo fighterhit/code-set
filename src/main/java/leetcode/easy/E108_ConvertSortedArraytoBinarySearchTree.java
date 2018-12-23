a8d6ca8fc105413ff42932a5ba05afac20581807
fighter <fighterhit@163.com>
2018-12-23 23:39:22 +0800

    add solutions

---
+++ src/main/java/leetcode/easy/E108_ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,47 @@
+package leetcode.easy;
+
+/**
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ * Example:
+ * Given the sorted array: [-10,-3,0,5,9],
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+ * 0
+ * / \
+ * -3   9
+ * /   /
+ * -10  5
+ *
+ * @author Fighter.
+ * 中序遍历一棵BST得到的就是从小到大的有序数组
+ */
+public class E108_ConvertSortedArraytoBinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
+    }
+}
---
+++ src/main/java/leetcode/middle/M109_ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,60 @@
+package leetcode.middle;
+
+/**
+ * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ * Example:
+ * Given the sorted linked list: [-10,-3,0,5,9],
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+ *  0
+ * / \
+ * -3   9
+ * /   /
+ * -10  5
+ *
+ * @author Fighter.
+ */
+public class M109_ConvertSortedListtoBinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        return sortedListToBST(head, null);
+    }
+
+    private TreeNode sortedListToBST(ListNode head, ListNode tail) {
+        if (head == null || head == tail){
+            return null;
+        }
+        ListNode fast= head,slow = head;
+        //注意这里是与tail作比较不是null，单链表找中点是与null比较
+        while (fast.next!= tail && fast.next.next!=tail){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head,slow);
+        root.right = sortedListToBST(slow.next,tail);
+        return root;
+    }
+}
