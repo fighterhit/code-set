7d0ffdf7ef381b1cc2fb4dfe82847a7ff2a9b15b
fighter <fighterhit@163.com>
2018-12-26 23:22:21 +0800

    add solution

--- src/main/java/leetcode/easy/E404_SumofLeftLeaves.java
+++ src/main/java/leetcode/easy/E404_SumofLeftLeaves.java
@@ -1,13 +1,15 @@
 package leetcode.easy;
 
+import java.util.Stack;
+
 /**
  * Find the sum of all left leaves in a given binary tree.
  * Example:
- * 3
- * / \
+ *   3
+ *  / \
  * 9  20
- * / \
- * 15  7
+ *   / \
+ *  15  7
  * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  *
  * @author Fighter.
@@ -41,27 +43,62 @@ public class E404_SumofLeftLeaves {
         return root.left == null && root.right == null;
     }
 
-    /*public int sumOfLeftLeaves2(TreeNode root) {
+    public int sumOfLeftLeaves2(TreeNode root) {
         if (root == null) {
             return 0;
+        }
+        return helper(root);
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        //判断左孩子是否为左叶子：当前节点非空，且其左孩子的左孩子为null，左孩子的右孩子也为null
+        //不能直接return，否则无法遍历右孩子
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            res += root.left.val;
+        }
+        return res + helper(root.left) + helper(root.right);
+    }
+
+    //非递归：栈，思想与上面判断叶子节点方法相同
+    public int sumOfLeftLeaves3(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) {
+                continue;
+            }
+            if (node.left != null && node.left.left == null && node.left.right == null) {
+                sum += node.left.val;
+            }
+            stack.push(node.right);
+            stack.push(node.left);
+        }
+        return sum;
+    }
+
+    public int sumOfLeftLeaves4(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return helper(root, false);
+    }
+
+    int helper(TreeNode root, boolean isLeft) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null && isLeft) {
+            return root.val;
         }
         return helper(root.left, true) + helper(root.right, false);
     }
-
-    int helper(TreeNode root, boolean flag) {
-        if (root == null) {
-            return 0;
-        }
-        while (root.left != null) {
-            root = root.left;
-        }
-        if (root.right == null) {
-            if (flag) {
-                return root.val;
-            }
-            return 0;
-        } else {
-            return helper(root.right, false);
-        }
-    }*/
 }
