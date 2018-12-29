bfac3f0be721cb75c67123e2e7bd6286e2404342
fighter <fighterhit@163.com>
2018-12-29 23:33:57 +0800

    init solutions

---
+++ src/main/java/leetcode/middle/M450_DeleteNodeinaBST.java
@@ -0,0 +1,25 @@
+package leetcode.middle;
+
+/**
+ * 删除的不存在
+ * 存在多个需要删除的
+ * 是否需要返回
+ */
+public class M450_DeleteNodeinaBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+    }
+}
