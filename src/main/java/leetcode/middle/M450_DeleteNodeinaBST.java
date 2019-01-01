81a1ab54485eaf3d9d3e025e904c0721e8456948
fighter <fighterhit@163.com>
2019-01-02 00:09:02 +0800

    Merge branch 'master' of /Users/fighter/IdeaProjects/code-set with conflicts.

--- src/main/java/leetcode/middle/M450_DeleteNodeinaBST.java
+++ src/main/java/leetcode/middle/M450_DeleteNodeinaBST.java
@@ -1,9 +1,11 @@
 package leetcode.middle;
 
 /**
- * 删除的不存在
- * 存在多个需要删除的
- * 是否需要返回
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+ * Basically, the deletion can be divided into two stages:
+ * Search for a node to remove.
+ * If the node is found, delete the node.
+ * Note: Time complexity should be O(height of tree).
  */
 public class M450_DeleteNodeinaBST {
     public class TreeNode {
@@ -17,51 +19,20 @@ public class M450_DeleteNodeinaBST {
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
-        }
-        TreeNode cur = root, pre = null;
-        while (cur != null && cur.val != key) {
-            pre = cur;
-            if (cur.val > key) {
-                cur = cur.left;
+        TreeNode tmp = root, node;
+        while (true) {
+            if (root == null) {
+                return null;
+            }
+            if (key < root.val) {
+                root = root.left;
+            } else if (key > root.val) {
+                root = root.right;
             } else {
-                cur = cur.right;
+                node = root;
+                break;
             }
         }
-        //当前树的根节点即为待删除节点
-        if (pre == null) {
-            return deleteNodeHelper(cur);
-        }
-        if (pre.left == cur) {
-            pre.left = deleteNodeHelper(cur);
-        } else {
-            pre.right = deleteNodeHelper(cur);
-        }
-        return root;
-    }
-
-    TreeNode deleteNodeHelper(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        if (root.left == null) {
-            return root.right;
-        }
-        if (root.right == null) {
-            return root.left;
-        }
-        //把待删节点的左子树 挂到 右子树中最小节点的左孩子
-        TreeNode rightMin = findMin(root.right);
-        rightMin.left = root.left;
-        return root.right;
-
-    }
-
-    TreeNode findMin(TreeNode root) {
-        while (root.left != null) {
-            root = root.left;
-        }
-        return root;
+        
     }
 }
