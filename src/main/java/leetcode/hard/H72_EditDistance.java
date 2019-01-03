55c57515d8c2acc907e3c6f432c39f9021f60e35
fighter <fighterhit@163.com>
2019-01-03 23:38:41 +0800

    init solutions

---
+++ src/main/java/leetcode/hard/H72_EditDistance.java
@@ -0,0 +1,32 @@
+package leetcode.hard;
+
+/**
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+ * You have the following 3 operations permitted on a word:
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * <p>
+ * Example 1:
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * <p>
+ * Example 2:
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ */
+public class H72_EditDistance {
+    public int minDistance(String word1, String word2) {
+
+    }
+}
