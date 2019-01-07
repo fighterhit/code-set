177960957db25a0f7f18f5536309cb6cf7c78bd7
fighter <fighterhit@163.com>
2019-01-07 23:23:20 +0800

    init solutions

---
+++ src/main/java/leetcode/middle/M22_GenerateParentheses.java
@@ -0,0 +1,20 @@
+package leetcode.middle;
+
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * For example, given n = 3, a solution set is:
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+public class M22_GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+
+    }
+}
