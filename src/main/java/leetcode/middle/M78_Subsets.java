d5bba0ddcf1a7a0aa8e1a86310e0b5b0a80143f7
fighter <fighterhit@163.com>
2019-01-15 23:26:14 +0800

    add solutions

---
+++ src/main/java/leetcode/middle/M78_Subsets.java
@@ -0,0 +1,28 @@
+package leetcode.middle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ * Note: The solution set must not contain duplicate subsets.
+ * <p>
+ * Example:
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ */
+public class M78_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+
+    }
+}
---
+++ src/main/java/leetcode/middle/M90_SubsetsII.java
@@ -0,0 +1,25 @@
+package leetcode.middle;
+
+import java.util.List;
+
+/**
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+ * Note: The solution set must not contain duplicate subsets.
+ * <p>
+ * Example:
+ * Input: [1,2,2]
+ * Output:
+ * [
+ * [2],
+ * [1],
+ * [1,2,2],
+ * [2,2],
+ * [1,2],
+ * []
+ * ]
+ */
+public class M90_SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+    }
+}
